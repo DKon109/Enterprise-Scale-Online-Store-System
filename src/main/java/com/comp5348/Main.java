62cf762b4eea48dbb6aa452646e84e0b008c9e00
Babski <bbabin101@gmail.com>
2025-10-11 10:35:32 +1100

    Test commit to new branch.

--- src/main/java/com/comp5348/Main.java
+++ src/main/java/com/comp5348/Main.java
@@ -1,13 +1,13 @@
 package com.comp5348;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-@SpringBootApplication
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("Hello World! Brandon's version.");
+        }
+
     }
 }
