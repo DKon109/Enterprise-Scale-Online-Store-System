d835bf74ec8bf5f87bb64e8bfe203aeeceea63f5
Ryoji Kondo <rkon0071@uni.sydney.edu.au>
2025-10-31 19:28:57 +1100

    Feature/warehouse to main (#2)
    * Implement inventory and warehouse
    
    * Implement inventory and warehouse
    
    * feat: update inventory and warehouse logic

--- src/main/java/com/comp5348/store/inventory/controller/InventoryController.java
+++ src/main/java/com/comp5348/store/inventory/controller/InventoryController.java
@@ -1,4 +1,36 @@
 package com.comp5348.store.inventory.controller;
 
+import com.comp5348.store.inventory.model.Inventory;
+import com.comp5348.store.inventory.service.InventoryService;
+import com.comp5348.store.inventory.service.InventoryService.Allocation;
+
+import java.util.*;
+
 public class InventoryController {
+
+    private final InventoryService inventoryService;
+    public InventoryController(InventoryService invSer) {
+        this.inventoryService = invSer;
+    }
+
+    //GET / inventory
+    public Collection<Inventory> listAllInventory() {
+        return inventoryService.viewAllInventory();
+    }
+
+    // POST /inventory/reserve?productId={productId}&qty={qty}
+    public Optional<List<Allocation>> reserve(Long productId, int qty){
+        return inventoryService.reserveProduct(productId, qty);
+    }
+
+    //POST/ inventory/ commit (body: allocations)
+    public boolean commit(List<Allocation> allocations){
+        return inventoryService.commitReservation(allocations);
+    }
+
+    //POST / inventory/ release (body: allocations)
+    public boolean release(List<Allocation> allocations){
+        return inventoryService.releaseReservation(allocations);
+    }
 }
+
--- src/main/java/com/comp5348/store/inventory/model/Inventory.java
+++ src/main/java/com/comp5348/store/inventory/model/Inventory.java
@@ -1,4 +1,79 @@
 package com.comp5348.store.inventory.model;
 
 public class Inventory {
+    private Long warehouseId;
+    private Long productId;
+    private int quantityAvailable;
+    private int quantityReserved;
+
+    public Inventory(Long warehouseId, Long productId, int quantityAvailable, int quantityReserved) {
+        this.warehouseId = warehouseId;
+        this.productId = productId;
+        this.quantityAvailable = quantityAvailable;
+        this.quantityReserved = quantityReserved;
+    }
+
+    public Long getWarehouseId() {
+        return warehouseId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public int getQuantityAvailable() {
+        return quantityAvailable;
+    }
+
+    public int getQuantityReserved() {
+        return quantityReserved;
+    }
+
+    public void setQuantityAvailable(int quantityAvailable) {
+        this.quantityAvailable = quantityAvailable;
+    }
+
+    public void setQuantityReserved(int quantityReserved) {
+        this.quantityReserved = quantityReserved;
+    }
+
+    //domain helpers
+
+    //**
+    // reserve qty items: reduce quantityAvailable and increase quantityReserved
+    // use the boolean to check if it goes through*/
+    public boolean reserve(int qty) {
+        if (qty <= 0) return false;
+        if (quantityAvailable < qty){
+            return false;
+        }
+        quantityAvailable -= qty;
+        quantityReserved += qty;
+        return true;
+    }
+
+    /** release the reserved quantity: reduce the quantityReserved and increase quantityAvailable */
+    public boolean release(int qty) {
+        if (qty <= 0) return false;
+        if (quantityReserved < qty){
+            return false;
+        }
+        quantityReserved -= qty;
+        quantityAvailable += qty;
+        return true;
+    }
+
+    /**
+     * commit the reserved stock. Since it will proceed to delivery, it will reduce "reserved".
+     * don't put it back to "available".
+     */
+    public boolean commitReserved(int qty) {
+        if (qty <= 0) return false;
+        if (quantityReserved < qty){
+            return false;
+        }
+        quantityReserved -= qty;
+        //available is already reduced
+        return true;
+    }
 }
