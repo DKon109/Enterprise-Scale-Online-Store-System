35618612475ba788408c953f1167b81b4f56ba51
Brandon Babin <bbab0601@uni.sydney.edu.au>
2025-11-02 16:11:59 +1100

    PaymentTransaction feature (#6)
    * Test commit to new branch.
    
    * Added basic structure of paymentTransaction class. Included springboot application dependencies in gradle build file.
    
    * further changes to PaymentTransaction implementation, including controller, repo and service. Bare minimum implementation of Order included.
    
    ---------
    
    Co-authored-by: Babski <bbabin101@gmail.com>

--- src/main/java/com/comp5348/Main.java
+++ src/main/java/com/comp5348/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 0; i < 5; i++){
-            System.out.println("Hello World!");
+            System.out.println("Hello World! Brandon's version.");
         }
 
     }
--- src/main/java/com/comp5348/store/order/model/Order.java
+++ src/main/java/com/comp5348/store/order/model/Order.java
@@ -1,4 +1,12 @@
 package com.comp5348.store.order.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
 }
