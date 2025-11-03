b95ae39c686a74f7a4a3bad643b0a3dfa9890561
Babski <bbabin101@gmail.com>
2025-11-03 18:39:48 +1100

    feat: basic functionality around paymentTransactions.

---
+++ src/main/java/com/comp5348/bank/dto/PaymentTransactionDTO.java
@@ -0,0 +1,34 @@
+package com.comp5348.bank.dto;
+
+import com.comp5348.bank.model.PaymentTransaction;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Data Transfer Object for PaymentTransaction
+ */
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PaymentTransactionDTO {
+    private long id;
+    private Double amount;
+    private String type;
+    private String status;
+    private String bankReferenceID;
+    private LocalDateTime time;
+    private UUID orderID;
+
+    public PaymentTransactionDTO(PaymentTransaction entity) {
+        this.id = entity.getId();
+        this.amount = entity.getAmount();
+        this.type = entity.getType();
+        this.status = entity.getStatus();
+        this.bankReferenceID = entity.getBankReferenceID();
+        this.orderID = entity.getOrderID();
+    }
+}
